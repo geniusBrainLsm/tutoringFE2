7b5d302ebe976b7357737811fb4d95ad17368182
컴퓨터\kim-jin-young <wlsdud11457@gmail.com>
2024-02-13 17:21:10 +0900

    Add - 로그인 기능 추가

--- frontend/src/component/SignInModal.jsx
+++ frontend/src/component/SignInModal.jsx
@@ -1,12 +1,48 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Modal from 'react-bootstrap/Modal';
 import '../css/SignIn.css';
 import googleBtn from '../img/web_neutral_sq_na@1x.png';
 import kakaoBtn from '../img/kakaotalk_sharing_btn_small.png'
 import naverBtn from '../img/btnG_icon_square.png'
 import testLogo from '../img/size_s_icon_137187.png'
+import {useNavigate} from "react-router-dom";
+import { signIn } from "../util/APIUtils";
 function SignInModal(props) {
 
+    const navigate = useNavigate();
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
+
+        // 각각의 입력 필드에 대해 상태 업데이트
+        if (email === 'email') {
+            setEmail(value);
+        } else if (password === 'password') {
+            setPassword(value);
+        }
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        const formData = {
+            email: email,
+            password: password,
+        };
+
+        signIn(formData)
+            .then(() => {
+                navigate('/'); // 로그인 성공시 메인화면으로 이동
+                props.onHide();
+            })
+            .catch((error) => {
+                alert('SignIn Failed!');
+            });
+    };
+        
     return (
         <Modal
             {...props}
@@ -15,35 +51,37 @@ function SignInModal(props) {
         >
             <Modal.Body>
                 <div className={"form-background"}>
-                    <img className={"form-logo"} src={testLogo} alt='로고'/>
-                    <div>
-                        <input
-                            className={"form-input"}
-                            // value={}
-                            // onChange={}
-                            type={"text"}
-                            placeholder={"이메일"}
-                        />
-                    </div>
-                    <div>
-                        <input
-                            className={"form-input"}
-                            // value={}
-                            // onChange={}
-                            type={"text"}
-                            placeholder={"비밀번호"}
-                        />
-                    </div>
-                    <div>
-                        <button type={"submit"} className={"form-button"}>
-                            로그인
-                        </button>
-                    </div>
-                    <div className={"form-find-div"}>
-                        <a href="#" className={"form-find"}>아이디 찾기</a>
-                        <a href="#1" className={"form-find"}>비밀번호 찾기</a>
-                        <a href="/frontend/src/component/SignUpModal" className={"form-find"}>회원가입</a>
-                    </div>
+                    <form onSubmit={handleSubmit}>
+                        <img className={"form-logo"} src={testLogo} alt='로고'/>
+                        <div>
+                            <input
+                                className={"form-input"}
+                                value={email}
+                                onChange={handleInputChange}
+                                type={"text"}
+                                placeholder={"이메일"}
+                            />
+                        </div>
+                        <div>
+                            <input
+                                className={"form-input"}
+                                value={password}
+                                onChange={handleInputChange}
+                                type={"text"}
+                                placeholder={"비밀번호"}
+                            />
+                        </div>
+                        <div>
+                            <button type={"submit"} className={"form-button"}>
+                                로그인
+                            </button>
+                        </div>
+                        <div className={"form-find-div"}>
+                            <a href="#" className={"form-find"}>아이디 찾기</a>
+                            <a href="#1" className={"form-find"}>비밀번호 찾기</a>
+                            <a href="/frontend/src/component/SignUpModal" className={"form-find"}>회원가입</a>
+                        </div>
+                    </form>
                     <div className={"hr-sect"}>
                         간편로그인
                     </div>
